ca18c42752d341250d5a7265d4fb076445568d39
Gavin Cabbage <gavincabbage@gmail.com>
2016-10-20 22:06:55 +0000

    added feed module

--- bin/run_camera.sh
+++ bin/run_camera.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo ~/.virtualenvs/cv3/bin/gunicorn --timeout 3600 -w 1 -b 0.0.0.0:9090 --error-logfile - --access-logfile - feed:app
+sudo ~/.virtualenvs/cv/bin/gunicorn --timeout 3600 -w 1 -b 0.0.0.0:9090 --error-logfile - --access-logfile - feed:app
